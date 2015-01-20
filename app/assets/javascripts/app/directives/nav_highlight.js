f4274cc68bf63c6175df62f3500469552ea64bb2
Joshua Comeau <joshwcomeau@gmail.com>
2015-01-20 17:58:35 -0500

    Thorough refactoring and improvement of navHighlight directive.

--- app/assets/javascripts/app/directives/nav_highlight.js
+++ app/assets/javascripts/app/directives/nav_highlight.js
@@ -3,55 +3,41 @@ function navHighlight($window) {
     restrict: 'A',
     link: function(scope, element, attrs) {
       var navHeight     = $("body > nav").height(),
-          foundActive   = false,
-          windowTop;
+          sections, windowTop, activeSection;
 
-      $($window).on("scroll", function() {
-        foundActive = false;
+      $($window).on("scroll resize", function() {
+        _.debounce(setActive, 250)();
+      });
+
+
+      function findActive() {
         windowTop   = $(window).scrollTop();
-        sections    = createSectionArray(attrs.sections),
-          
+        sections    = createSectionArray(attrs.sections);
 
-        _.forEachRight(sections, function(section) {
-          if ( windowTop >= section.height && !foundActive ) {
+        return _.find(sections, function(section) {
+          if ( windowTop >= section.height ) {
             // We've found our active section!
-            foundActive = true;
-            console.log("Active section is ", section, " because window scrolltop is ", windowTop);
-            scope.$apply(function() {
-              scope.home.activeSection = section.name;
-            });
-            return true;
+            return section.name;;
           }
         });
+      }
 
-        // If we're still here, there is no active section. Set it to null.
-        if (!foundActive) {
-          console.log("No active =(")
-
-          scope.$apply(function() {
-            scope.home.activeSection = null;
-          });
-          return true;
-        }
-
-        console.log(scope.home);
-      });
-
-
-
-      
-      $($window).on("resize", function() {
-
-      });
+      function setActive() {
+        activeSection = findActive() || {name: null};
+        
+        scope.$apply(function() {
+          scope.home.activeSection = activeSection.name;
+        });
+      }
 
       function createSectionArray(sections) {
-        return _.map(sections.split(","), function(section) {
+        return _.map(sections.split(",").reverse(), function(section) {
           return { 
             name:   section, 
             height: $("."+section).offset().top - navHeight
           }
         });
-      }
+      }      
 
     }
   };
@@ -59,15 +45,3 @@ function navHighlight($window) {
 
 navHighlight.$inject = ['$window'];
 angular.module('joshwcc').directive('navHighlight', ['$window', navHighlight]);
-
-$(document).ready(function() {
-
-  $(window).scroll(function(e) {
-    windowTop = $(window).scrollTop()
-    console.log();
-
-    // When the window scrolltop hits the section's top (minus the header height), we want the section to activate.
-  });
-
-
-});
