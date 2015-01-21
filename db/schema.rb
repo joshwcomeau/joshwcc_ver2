016962cc2687a5b0932e0dfabfc31abe505f0a33
Joshua Comeau <joshwcomeau@gmail.com>
2015-01-21 15:11:44 -0500

    Added fields to Project

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150121134823) do
+ActiveRecord::Schema.define(version: 20150121200949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20150121134823) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "content"
+    t.string   "stack"
+    t.string   "colour"
+    t.string   "demo_link"
+    t.string   "github_link"
+    t.string   "project_type"
+    t.string   "project_length"
   end
 
 end
