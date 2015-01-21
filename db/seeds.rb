ea84ff2deceecfa236e8f0588552e33d505c5d83
Joshua Comeau <joshwcomeau@gmail.com>
2015-01-21 08:35:19 -0500

    Generated Project model, added seeds to populate it with our models.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+def normalize_name(name)
+  name.downcase.gsub(/[^\w]/i, '')
+end
+
+
+projects = [ "#paid", "HungryBelly", "ColourMatch", "CLYWmparison", "Percentext", "Schedulr", "Mars Rover", "This Portfolio", "Conway's Game of Life", "Earthy" ].reverse
+
+projects.each do |p|
+  puts "Creating project #{p}"
+
+  Project.create({
+    display_name: p,
+    thumb_path:   "#{normalize_name(p)}_thumb.png"
+  })
+  # Adding a sleep so that there is a clear difference in created_at times, so the order is preserved.
+  sleep 1
+end
+
+
