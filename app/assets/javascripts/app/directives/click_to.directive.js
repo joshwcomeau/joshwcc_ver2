881f945d5357c5cc03213becef2e28030e27bcc4
Joshua Comeau <joshwcomeau@gmail.com>
2015-01-22 08:08:25 -0500

    Improved click-nav animations, so that there isn't that awkward delay.

--- app/assets/javascripts/app/directives/click_to.directive.js
+++ app/assets/javascripts/app/directives/click_to.directive.js
@@ -8,6 +8,12 @@ function clickTo() {
       element.click(function() {
         newOffset = $("."+attrs.dest).offset().top;
         $("html, body").animate({ scrollTop: newOffset - navHeight });
+
+        // Add a temporary animation to the clicked element, after removing it from others
+        $(".main-nav-li").removeClass("clicked").removeClass("active");
+        element.addClass("clicked");
+
+
       })
     }
   };
