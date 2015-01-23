c8bd7e5057b6c7d565f0d4d9b921a5c2a87db817
Joshua Comeau <joshwcomeau@gmail.com>
2015-01-23 13:43:11 -0500

    BUGFIX: Fixed the view_project directive scroll height by fetching the height on every click, not on pageload.

--- app/assets/javascripts/app/directives/view_project.directive.js
+++ app/assets/javascripts/app/directives/view_project.directive.js
@@ -2,17 +2,20 @@ function viewProject(Project) {
   return {
     restrict: 'A',
     link: function(scope, element, attrs) {
-      var navHeight = $(".main-nav").height(),
-          projectOffset = $(".projects").offset().top;
+      var navHeight,
+          projectOffset;
 
       element.click(function() {
+        projectOffset = $(".projects").offset().top;
+        navHeight = $(".main-nav").height()
         // Scroll to the top of projects
+
         $("html, body").animate({ scrollTop: (projectOffset - navHeight) }, 250);
 
         // Set the project height equal to the child height.
         setTimeout(function() {
           console.log($(".active-project").height())
-        $("section.projects").height( $(".active-project").height() + 20 );
+        $("section.projects").height( $(".active-project").height() );
         }, 250);
         
       })
