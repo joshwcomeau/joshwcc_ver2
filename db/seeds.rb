2e5b997928d17ea13b5f66fb0ee8710edc1af590
Joshua Comeau <joshwcomeau@gmail.com>
2015-01-31 13:40:19 -0500

    Removed hardcoded author and dates, made a nice date formatter in ApplicationHelper.

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ ActiveRecord::Base.connection.tables.each { |t| ActiveRecord::Base.connection.re
 @blog_post_1 =  BlogPost.create({
   abstract: 'A few words describing the post, for the main site.',
   title: 'A Sample Blog Post',
-  author: 'Joshua Comeau',
+  author: 'The Indefatigable Joshua Comeau',
   content: "[quote class=\"red\"]Lorem Ipsum is simply dummy text of the printing and typesetting industry.[/quote] Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 })
 
