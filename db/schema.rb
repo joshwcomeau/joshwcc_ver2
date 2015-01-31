a2893633d76daf4f587c0d37fd08456bbe523f79
Joshua Comeau <joshwcomeau@gmail.com>
2015-01-31 16:25:34 -0500

    Added a bunch more fields to BlogPost

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150126190350) do
+ActiveRecord::Schema.define(version: 20150131204853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(version: 20150126190350) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.boolean  "featured"
+    t.string   "subtitle"
+    t.string   "colour"
+    t.boolean  "published"
+    t.datetime "published_at"
   end
 
   create_table "images", force: true do |t|
