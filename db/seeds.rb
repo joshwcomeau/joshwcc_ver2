897ce46b0f0324e97f618346b749f76c8c98fa5e
Joshua Comeau <joshwcomeau@gmail.com>
2015-01-31 10:59:57 -0500

    Used a helper to add background image on posts in the index that have one available.

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,7 +24,7 @@ User.destroy_all
   content: "[intro class=\"red\"]Lorem Ipsum is simply dummy text of the printing and typesetting industry.[/intro] Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 })
 
-@blog_post_2.images << Image.new(src: 'https://s3.amazonaws.com/joshwcc/detail_percentext.png', placement: 0)
+@blog_post_2.images << Image.new(src: 'https://s3.amazonaws.com/joshwcc/cat.jpg', placement: 0)
 
 
 @josh.blog_posts << @blog_post_1
