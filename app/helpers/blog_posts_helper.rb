3e02a6135e7a9614029a9dc1b35e265c32c2574b
Joshua Comeau <joshwcomeau@gmail.com>
2015-02-01 10:50:21 -0500

    BUGFIX: Dealt with the edge case of our post having less than the snippet length's number of characters.

--- app/helpers/blog_posts_helper.rb
+++ app/helpers/blog_posts_helper.rb
@@ -1,15 +1,19 @@
 module BlogPostsHelper
-  MINIMUM_CHARS_FOR_PREVIEW = 30
+  MINIMUM_CHARS_FOR_PREVIEW = 2000
   def prepare_for_display(content)
     # Shortcode processes our file, and then we turn '\n's into 'br's and 'p's.
     content = Shortcode.process(content)
     simple_format(content)
   end
 
+  # What makes a perfect snippet? I think it should be the first N characters, rounded up to the nearest paragraph.
   def display_snippet(content)
     content = prepare_for_display(content)
 
-    # What makes a perfect snippet? I think it should be the first N characters, rounded up to the nearest paragraph.
+    # If our entire post fits in the snippet, just parse the HTML and return it.
+    return content.html_safe if content.length <= MINIMUM_CHARS_FOR_PREVIEW
+      
+
     first_part = content[0...MINIMUM_CHARS_FOR_PREVIEW]
     remainder  = content[MINIMUM_CHARS_FOR_PREVIEW..-1]
 
