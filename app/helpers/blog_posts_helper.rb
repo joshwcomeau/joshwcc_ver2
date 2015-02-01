cb18612b81b9b386c2c0e7c7c0b3be27dbbb70a6
Joshua Comeau <joshwcomeau@gmail.com>
2015-02-01 16:55:10 -0500

    Organized stylesheets and made them apply to blog#show. Removed .html_safe from BlogHelper display methods

--- app/helpers/blog_posts_helper.rb
+++ app/helpers/blog_posts_helper.rb
@@ -3,7 +3,7 @@ module BlogPostsHelper
 
   # What makes a perfect snippet? I think it should be the first N characters, rounded up to the nearest paragraph.
   def display_snippet(content)
-    content = simple_format(content, {}, sanitize: false)
+    content = display_full(content)
 
     # If our entire post fits in the snippet, just parse the HTML and return it.
     return content if content.length <= MINIMUM_CHARS_FOR_PREVIEW
@@ -11,12 +11,11 @@ module BlogPostsHelper
 
     first_part = content[0...MINIMUM_CHARS_FOR_PREVIEW]
     remainder  = content[MINIMUM_CHARS_FOR_PREVIEW..-1]
-
     (first_part + remainder.split("</p>").first + "</p>")
 
   end
 
   def display_full(content)
-    prepare_for_display(simple_format(content, sanitize: false))
+    simple_format(content, {}, sanitize: false)
   end
 end
