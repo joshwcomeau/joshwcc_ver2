d55e9e7743d05dc36aef86117020f4800e1baed7
Josh Comeau <joshwcomeau@gmail.com>
2015-02-07 14:50:17 -0500

    Started implementing new AWS gem features

--- app/controllers/s3_images_controller.rb
+++ app/controllers/s3_images_controller.rb
@@ -1,6 +1,18 @@
 class S3ImagesController < ApplicationController
+  before_action :get_bucket,    only: [:create]
+
   def create
-    binding.pry
-    s3 = AWS::S3::Client.new
+    filename  = params["image"].original_filename
+    new_obj   = @bucket.objects[filename]
+    if new_obj.write(params["image"])
+      render json: { message: 'success!'}, status: 200
+    else
+      render json: { message: 'error!'}, status: 500
+    end
   end
+
+
+  private
+
+
 end
